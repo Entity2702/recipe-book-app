6ac34e7bd0a74e3fb3a901c86ab0f2214c671320
Entity2702 <mikigrzywa@gmail.com>
2026-08-04 18:31:24 +0200

    forgot to save store.jsx

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { recipesApi } from "./api/RecipesAPI";
+
 export const store = configureStore({
  reducer: {
-  
- }
+  [recipesApi.reducerPath]: recipesApi.reducer,
+ },
+
+ middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(recipesApi.middleware),
+ 
 });
