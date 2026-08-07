7e357b4e36df7046be8f87d1531613ff38bed798
Entity2702 <mikigrzywa@gmail.com>
2026-08-07 15:40:32 +0200

    reworked searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,17 +4,21 @@ import { loupe } from "../assets/images";
 function Searchbar({className, onSearch}){
  const input = useRef(null);
 
+ const handleSearch = () => {
+  const userSearch = input.current.value;
+   onSearch(userSearch);
+ }
+
  const onTyping = (event) => {
   if(event.key === "Enter") {
-   const userSearch = input.current.value;
-   onSearch(userSearch);
+   handleSearch();
   }
  }
 
  return(
   <div className={`flex items-center w-[320px] border rounded-box px-5 py-2 border-black focus-within:shadow-orange-custom ${className}`}>
    <img src={loupe} className="w-6.25 h-6.25" />
-   <input type="text" className="w-full pl-2.5 focus:outline-none overflow-hidden whitespace-nowrap" ref={input} onKeyDown={onTyping}/>
+   <input type="text" className="w-full pl-2.5 focus:outline-none overflow-hidden whitespace-nowrap" ref={input} onKeyDown={onTyping} onBlur={handleSearch}/>
   </div>
  )
 }
